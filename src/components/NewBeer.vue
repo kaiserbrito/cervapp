3b3552306d4ac09466452e26127ee1507a07d54f
Victor Guerra <victorbcguerra@gmail.com>
2018-01-25 13:24:04 -0200

    Implementing NewBeer view, and adding data to firebase

--- src/components/NewBeer.vue
+++ src/components/NewBeer.vue
@@ -1,14 +1,65 @@
 <template>
   <div id="new-beer">
     <h2>New Beer</h2>
+    <div class="container">
+      <form @submit.prevent="saveBeer" class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" class="datepicker" v-model="time" required>
+            <label>Data</label>
+          </div>
+        </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="text" v-model="name_beer" required>
+              <label>Nome da Cerveja</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="text" v-model="type_beer" required>
+              <label>Tipo da Cerveja</label>
+            </div>
+        </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="text" v-model="quantity_beer" required>
+              <label>Quantidade (l)</label>
+            </div>
+          </div>
+          <button type="submit" class="btn">Criar</button>
+          <router-link to="/" class="btn grey">Cancelar</router-link>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import db from "./firebaseInit.js";
+
 export default {
   name: "new-beer",
   data() {
-    return {};
+    return {
+      time: null,
+      name_beer: null,
+      type_beer: null,
+      quantity_beer: null
+    };
+  },
+  methods: {
+    saveBeer() {
+      db
+        .collection("beers")
+        .add({
+          time: this.time,
+          name_beer: this.name_beer,
+          type_beer: this.type_beer,
+          quantity_beer: this.quantity_beer
+        })
+        .then(docRef => this.$router.push("/"))
+        .catch(error => console.log(err));
+    }
   }
 };
 </script>
--- static/main.js
+++ static/main.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $(".button-collapse").sideNav();
+  $(".datepicker").pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: "Today",
+    clear: "Clear",
+    close: "Ok",
+    closeOnSelect: false // Close upon selecting a date,
+  });
 });
