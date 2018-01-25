d0e3ad97d7fbb60f1ae0622b0e967faa0b7a406c
Victor Guerra <victorbcguerra@gmail.com>
2018-01-25 12:59:55 -0200

    Implementing Dashboard view, fetching data from firebase db

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,15 +1,54 @@
 <template>
-  <div id="dashboard">
-    <h2>Dashboard</h2>
+  <div id="dashboard" class="container">
+    <h3>Dashboard</h3>
+    <table class="bordered highlight">
+      <thead>
+        <th>Data</th>
+        <th>Nome</th>
+        <th>Tipo</th>
+        <th>Quantidade (l)</th>
+      </thead>
+      <tbody>        
+        <tr v-for="beer in beers" v-bind:key="beer.id">
+          <td>{{ beer.time }}</td>
+          <td>
+          <router-link :to="{ name: 'view-beer', params: { id: beer.id } }">{{ beer.name_beer }}</router-link>
+          </td>
+          <td>{{ beer.type_beer }}</td>
+          <td>{{ beer.quantity_beer }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'dashboard',
-    data() {
-      return {}
-    }
-  }
+import db from "./firebaseInit.js";
 
+export default {
+  name: "dashboard",
+  data() {
+    return {
+      beers: []
+    };
+  },
+  created() {
+    db
+      .collection("beers")
+      .orderBy("time")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            id: doc.id,
+            name_beer: doc.data().name_beer,
+            quantity_beer: doc.data().quantity_beer,
+            type_beer: doc.data().type_beer,
+            time: doc.data().time
+          };
+          this.beers.push(data);
+        });
+      });
+  }
+};
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,63 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Dashboard from '@/components/Dashboard'
-import EditBeer from '@/components/EditBeer'
-import EditRecipe from '@/components/EditRecipe'
-import NewBeer from '@/components/NewBeer'
-import NewRecipe from '@/components/NewRecipe'
-import Recipes from '@/components/Recipes'
-import Stock from '@/components/Stock'
-import ViewBeer from '@/components/ViewBeer'
-import ViewRecipe from '@/components/ViewRecipe'
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "@/components/Dashboard";
+import EditBeer from "@/components/EditBeer";
+import EditRecipe from "@/components/EditRecipe";
+import NewBeer from "@/components/NewBeer";
+import NewRecipe from "@/components/NewRecipe";
+import Recipes from "@/components/Recipes";
+import Stock from "@/components/Stock";
+import ViewBeer from "@/components/ViewBeer";
+import ViewRecipe from "@/components/ViewRecipe";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [{
-      path: '/',
-      name: 'dashboard',
+  routes: [
+    {
+      path: "/",
+      name: "dashboard",
       component: Dashboard
     },
     {
-      path: '/edit-beer/:beer_id',
-      name: 'edit-beer',
+      path: "/edit-beer/:id",
+      name: "edit-beer",
       component: EditBeer
     },
     {
-      path: '/edit-recipe/:recipe_id',
-      name: 'edit-recipe',
+      path: "/edit-recipe/:id",
+      name: "edit-recipe",
       component: EditRecipe
     },
     {
-      path: '/new-beer',
-      name: 'new-beer',
+      path: "/new-beer",
+      name: "new-beer",
       component: NewBeer
     },
     {
-      path: '/new-recipe',
-      name: 'new-recipe',
+      path: "/new-recipe",
+      name: "new-recipe",
       component: NewRecipe
     },
     {
-      path: '/recipes',
-      name: 'recipes',
+      path: "/recipes",
+      name: "recipes",
       component: Recipes
     },
     {
-      path: '/stock',
-      name: 'stock',
+      path: "/stock",
+      name: "stock",
       component: Stock
     },
     {
-      path: '/view-beer/:beer_id',
-      name: 'view-beer',
+      path: "/view-beer/:id",
+      name: "view-beer",
       component: ViewBeer
     },
     {
-      path: '/view-recipe/:recipe_id',
-      name: 'view-recipe',
+      path: "/view-recipe/:id",
+      name: "view-recipe",
       component: ViewRecipe
     }
   ]
-})
+});
