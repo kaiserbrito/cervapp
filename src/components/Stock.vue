11dc49a84d86a0054cb005270d4c0ff578efbd20
Victor Guerra <victorbcguerra@gmail.com>
2018-01-26 11:19:21 -0200

    Implementing Stock view

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,15 +1,51 @@
 <template>
-  <div id="stock">
-    <h2>Stock</h2>
+  <div id="stock" class="container">
+    <h3>Estoque</h3>
+    <table class="bordered highlight">
+      <thead>
+        <th>Quantidade</th>
+        <th>Nome</th>        
+      </thead>
+      <tbody>        
+        <tr v-for="supply in supplies" v-bind:key="supply.id">
+          <td>{{ supply.quantity_supply }}</td> 
+          <td>{{ supply.name_supply }}</td>                   
+        </tr>
+      </tbody>
+    </table>
+    <div class="fixed-action-btn">
+      <router-link to="/" class="btn-floating btn-large red">
+        <i class="fa fa-plus"></i>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import db from "./firebaseInit.js";
+
 export default {
   name: "stock",
   data() {
-    return {};
+    return {
+      supplies: []
+    };
+  },
+  created() {
+    db
+      .collection("supplies")
+      .orderBy("quantity_supply")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            id: doc.id,
+            name_supply: doc.data().name_supply,
+            quantity_supply: doc.data().quantity_supply
+          };
+          this.supplies.push(data);
+        });
+      });
   }
 };
 </script>
-
